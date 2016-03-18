99bacc21e7d0020ae7cd41f6d3012413dca40e7c
mdronamr <mdronamr@douglas.co.us>
2016-03-17 22:54:23 -0600

    svtco

---
+++ web/js/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var App = angular.module('myApp',['ngRoute']);
+
+App.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+        .when('/home', {
+            templateUrl: 'views/main.html'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html'
+        })
+        .when('/events', {
+            templateUrl: 'views/events.html'
+        })
+        .when('/donate', {
+            templateUrl: 'views/donate.html'
+        })
+        .when('/services', {
+            templateUrl: 'views/services.html'
+        })
+        .when('/school', {
+            templateUrl: 'views/school.html'
+        })
+        .when('/contactUs', {
+            templateUrl: 'views/contactUs.html'
+        })
+        .when('/register', {
+            templateUrl: 'views/register.html'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html'
+        })
+		.otherwise({redirectTo:'/home'});
+}]);
