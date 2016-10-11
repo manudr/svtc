2ce110b6b2db715ad236c2c6c32c493c0b9b822c
mdronamr <mdronamr@douglas.co.us>
2016-10-11 14:19:08 -0600

    flyer

--- existing/index.php
+++ existing/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <meta http-equiv="refresh" content="0;url=homepage.php"/>
+        <meta http-equiv="refresh" content="15;url=homepage.php"/>
 
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -14,6 +14,10 @@
 				background: -o-linear-gradient(red, yellow, green); /* For Opera 11.1 to 12.0 */
 				background: -moz-linear-gradient(red, yellow, green); /* For Firefox 3.6 to 15 */
 				background: linear-gradient(red, yellow, green); /* Standard syntax */
+			}
+
+			.blackBg {
+				background: #000000; /* For browsers that do not support gradients */
 			}
 
 			.headerStyle {
@@ -33,9 +37,24 @@
 		</style>
     </head>
 
-    <body>
+	<body class="blackBg">
 
+	<div class="row">
+		<div class="col-lg-12 headerStyle">
+			You will be redirected in 15 seconds...
+			Please click <a href="//www.svtempleco.org/homepage.php"><b style="color: #FFFFFF; font-style: oblique; stroke: 1px; ">here</b></a> to skip.
+		</div>
+	</div>
+	<br>
+	<div class="row">
+		<div class="col-lg-5">
+			<img class="center-block" src="flyers/flyer_1.1.jpg" width="100%">
+		</div>
+		<div class="col-lg-7">
+			<img class="center-block" src="flyers/flyer_1.2.png" width="100%">
+		</div>
+	</div>
 
-    </body>
+	</body>
 
 </html>
