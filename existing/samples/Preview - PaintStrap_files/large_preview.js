04a26f963ac891fe0a9a4cf79e17ae3be2f87fd1
mdronamr <mdronamr@douglas.co.us>
2017-01-17 12:38:28 -0700

    adding to GIT

---
+++ existing/samples/Preview - PaintStrap_files/large_preview.js
@@ -0,0 +1,22 @@
+$(function(){
+	$(window).resize(function() {
+		updateLargePreviewSize();
+	});
+	
+	$("#design-changer").change(function() {
+		updateLargePreviewContents($(this).val());
+	});
+	
+	updateLargePreviewSize();
+	updateLargePreviewContents(default_design);
+});
+
+function updateLargePreviewSize() {
+	$("#large-preview").css("width", "100%").css("height", ($(window).height() - 51) + "px");
+}
+
+function updateLargePreviewContents(design) {
+	$("#large-preview").attr("src", base_url + "preview_large" + 
+			"?url_bootstrap_min_css=" + encodeURI(url_bootstrap_min_css) +
+			"&design=" + encodeURI(design));
+}
