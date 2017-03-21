e34846e028cb70af87111bd5a2971dc87a064f2f
mdronamr <mdronamr@douglas.co.us>
2017-03-20 22:22:09 -0600

    adding to GIT

--- JSF2Hibernate4/src/main/java/com/dronamraju/svtemple/service/UserService.java
+++ JSF2Hibernate4/src/main/java/com/dronamraju/svtemple/service/UserService.java
@@ -40,4 +40,8 @@ public class UserService {
         return userDAO.orderNumberExists(orderNumber);
     }
 
+    public String findValue(String name) {
+        return userDAO.findValue(name);
+    }
+
 }
