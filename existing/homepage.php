9247bf3d49947a02d735702fa5a2d0086150ff54
mdronamr <mdronamr@douglas.co.us>
2017-05-06 12:00:30 -0600

    Removed MySql Database Connection

--- existing/homepage.php
+++ existing/homepage.php
@@ -199,6 +199,7 @@ flashvars="height=20&width=55&file=NAMO_VENKATESAYA.mp3&volume=1&autostart=true&
 
 
 // Make the connnection and then select the database.
+/*
 $dbc = @mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) OR die ('Could not connect to MySQL: ' . mysql_error() );
 mysql_select_db (DB_NAME) OR die ('Could not select the database: ' . mysql_error() );
 $query="SELECT * FROM announcements order by vieworder";
@@ -228,6 +229,8 @@ mysql_close();
 		}
 		$i++;
 	}
+
+*/
 /*print "expirydate {$expirydate}";
 print "publishdate {$publishdate} ";
 print "currentdate {$currentdate}";*/
